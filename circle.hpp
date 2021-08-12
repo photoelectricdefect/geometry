21f0f6eac431a82a6dd4e1dbd32b1dfc00206a85
photoelectricdefect <ng1805@student.uni-lj.si>
2021-08-12 03:55:38 +0200

    Added descriptions

--- circle.hpp
+++ circle.hpp
@@ -3,6 +3,11 @@
 
 #include <Eigen/Dense>
 
+/**
+ * @brief  Utility class for operating with circles
+ * @note   
+ * @retval None
+ */
 class circle
 {
 private:
@@ -11,9 +16,24 @@ public:
     Eigen::Vector2d S;
     double r;
 
+    /**
+     * @brief  Constructor
+     * @note   
+     * @param  S_: Center
+     * @param  r_: Radius
+     * @retval 
+     */
     circle(Eigen::Vector2d S_,double r_);
     ~circle();
 
+    /**
+     * @brief  Fits the circle to 3 points that lie on it
+     * @note   
+     * @param  A: Point
+     * @param  B: Point
+     * @param  C: Point
+     * @retval New circle object fit to points
+     */
     static circle fit(const Eigen::Vector2d& A,const Eigen::Vector2d& B,const Eigen::Vector2d& C);
 };
 
